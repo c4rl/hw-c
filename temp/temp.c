f60ed8b6217b9f06721066a90a89e2c8cd120cfa
Carl Wiedemann <carl.wiedemann@gmail.com>
2018-08-23 21:30:11 -0600

    constants

--- temp/temp.c
+++ temp/temp.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+#define LOWER_F 0
+
+#define UPPER_F 300
+
+#define STEP_F 20
+
 /**
  * Converts F to C.
  * 
@@ -21,11 +27,7 @@ int main()
   float index_f, c;
   int lower_f, upper_f, step_f;
 
-  lower_f = 0;
-  upper_f = 300;
-  step_f = 20;
-
-  for (index_f = upper_f; index_f >= lower_f; index_f -= step_f) {
+  for (index_f = UPPER_F; index_f >= LOWER_F; index_f -= STEP_F) {
     printf("%3.0f\t%6.1f\n", index_f, convert_f_to_c(index_f));
   }
 
