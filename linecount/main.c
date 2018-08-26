6634303065f9ae9e394dd207507c60c987c161d6
Carl Wiedemann <carl.wiedemann@gmail.com>
2018-08-25 23:07:40 -0600

    DEMORGAN

--- linecount/main.c
+++ linecount/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int c, nl;
+  int c, nl, last_char;
 
   nl = 0;
-  while ((c = getchar()) != EOF)
-    if (c == '\n' || c == ' ' || c == '\t')
-      ++nl;
+  while ((c = getchar()) != EOF) {
+    if (c != ' ' || last_char != ' ') {
+      putchar(c);
+    }
+    last_char = c;
+  }
 
   printf("%d\n", nl);
 }
