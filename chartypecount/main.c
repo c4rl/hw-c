f13badacdbdfbf723995ca03e543a78097d213f5
Carl Wiedemann <carl.wiedemann@gmail.com>
2018-08-27 11:25:17 -0600

    wip

---
+++ chartypecount/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  int c, i, count_white, count_other;
+  int counts_digit[10];
+
+  // Initialize counts;
+  count_white = count_other = 0;
+  for (i = 0; i < 10; i++) {
+    counts_digit[i] = 0;
+  }
+
+  while ((c = getchar()) != EOF) {
+
+  }
+
+
+
+}
