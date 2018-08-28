4433c778ea2033364d9ee76013951fd4de9d72fd
Carl Wiedemann <carl.wiedemann@gmail.com>
2018-08-27 21:52:46 -0600

    vertical histogram is more interesting

--- length_histogram/main.c
+++ length_histogram/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-#define MAX_LENGTH 20
+#define MAX_LENGTH 15
 
 int main() {
   int c, i, j, current_word_length;
   // Assume words may be up to 20 characters in length.
   int lengths[MAX_LENGTH + 1];
+
+  // Some values for the vertical histogram.
+  int vh_index, vh_max;
 
   // Initialize values.
   current_word_length = 0;
@@ -31,6 +34,7 @@ int main() {
   }
 
   // Print out the results, first as JSON.
+  printf("## AS JSON ##\n");
   printf("{");
   for (i = 1; i < MAX_LENGTH + 1; i++) {
     printf("%d: %d", i, lengths[i]);
@@ -39,8 +43,10 @@ int main() {
     }
   }
   printf("}\n");
+  printf("\n");
 
-  // Ok, now print as a histogram.
+  printf("## AS HORIZONTAL HISTOGRAM ##\n");
+  // Ok, now print as a horizontal histogram.
   for (i = 1; i < MAX_LENGTH + 1; i++) {
     printf("%d: ", i);
     for (j = 0; j < lengths[i]; j++) {
@@ -48,5 +54,41 @@ int main() {
     }
     printf("\n");
   }
+  printf("\n");
 
+  // Next, we want to make a vertical histogram. This is going to be more challenging.
+  printf("## AS VERTICAL HISTOGRAM ##\n");
+  // The width of the histogram is the MAX_LENGTH.
+  // The height of the histogram is the maximum count.
+  vh_max = 0;
+  for (i = 1; i < MAX_LENGTH + 1; i++) {
+    if (lengths[i] > vh_max) {
+      vh_max = lengths[i];
+    }
+  }
+
+  for (j = 0; j < vh_max; j++) {
+    for (i = 1; i < MAX_LENGTH + 1; i++) {
+      // As items are printed, a mark should only be printed if it in fact reaches the
+      // pertinent line, which will exist in descending order.
+      // These will be 3 characters wide.
+      if ((vh_max - j) <= lengths[i]) {
+        printf("  #");
+      }
+      else {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+  // Print a divider
+  for (i = 1; i < MAX_LENGTH + 1; i++) {
+    printf(" --");
+  }
+  printf("\n");
+  // Finally, print labels below.
+  for (i = 1; i < MAX_LENGTH + 1; i++) {
+    printf("% 3d", i);
+  }
+  printf("\n");
 }
